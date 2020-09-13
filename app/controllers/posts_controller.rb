7d48b0b55496710f1c368f6c6bf9a3f80bd2813c
Sourav Goswami <souravgoswami@protonmail.com>
2020-09-13 16:40:52 +0530

    Edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: %i(show)
+	before_action :set_post, only: %i(show edit update destroy)
 
 	def index
 		@posts = Post.all.order('created_at DESC')
@@ -19,6 +19,22 @@ class PostsController < ApplicationController
 	end
 
 	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
 	end
 
 	private
